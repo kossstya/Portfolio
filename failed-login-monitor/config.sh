a68d044ca7389fabe67dfa7d924cb6e80912ccf3
kossstya <internetarchitecture2025@gmail.com>
2026-06-03 14:47:57 +0300

    Delete failed-login-monitor directory

--- failed-login-monitor/config.sh
+++
@@ -1,8 +0,0 @@
-# Path to SSH auth log
-AUTH_LOG="/var/log/auth.log"
-
-# Flag IP if it has this many failed attempts or more
-ATTEMPT_THRESHOLD=5
-
-# Where to store daily reports
-REPORT_DIR="$(dirname "$0")/reports"
